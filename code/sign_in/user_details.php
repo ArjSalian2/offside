8d5954db1205c20004b8e8e792be86aeec27af96
Mahmood-Uni <131663241+Mahmood-Uni@users.noreply.github.com>
2023-12-08 03:24:54 +0000

    Update user_details.php
    Added the nav bar to the page as well as rearranged the CSS. Added an error message if user tries to change the email to an email that already exists in the database to prevent duplicates.

--- code/sign_in/user_details.php
+++ code/sign_in/user_details.php
@@ -61,17 +61,120 @@ unset($_SESSION['password_error']);
             height: 100vh;
         }
 
-        .details-container {
-            padding: 20px;
-            max-width: 400px;
-            width: 100%;
-            margin: auto;
-            margin-top: 50vh;
+        .page-container{
+            position: relative;
+            bottom: 250px;
         }
 
+        header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 10px 20px;
+        }
+
+        .top-right-nav{
+        margin-left: auto;
+        background-color:rgb(255, 255, 255);
+        /* flex-grow: 1; */
+        } 
+
+        .logo img { 
+        max-width: 100px;
+        height: auto;
+        /* position: absolute;
+        top:0;
+        left:0;
+        padding:5px; */
+        }
+
+        /* Dima- nav,basket,search bar */
+        #nav1{
+        display:flex;
+        align-items: center;
+        gap: 10px;
+        padding: 11px;
+        margin-left: 850px; 
+        background-color:white;
+        }
+
+        #nav1 a{
+        text-decoration: none;
+        color: rgb(11,10,10);
+        font-size: 13px;
+        text-align: center;
+        padding:3px 10px;
+        /* margin: 0 5px; */
+        }
+        #nav1 a:hover{
+        background-color: transparent;
+        color: darkgray;
+        }
+
+        #nav2{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: white;
+        padding: 10px;
+        box-sizing:border-box;
+        }
+        #nav2 a:hover{
+        background-color: transparent;
+        color: darkgray;
+        }
+        #nav2 a{
+        text-decoration: none;
+        color:black;
+        text-align: center;
+        padding: 5px 15px;
+        font-size: 14px;
+        }
+        .nav2-center{
+        flex-grow:1;
+        display: flex;
+        justify-content: center;
+        margin-left: 300px;
+        }
+        .nav2-right{
+        display: flex;
+        align-items: center;
+        }
+        
+        #search form{
+        display:flex;
+        align-items: center;
+        border:1px solid #ccc;
+        border-radius: 15px;
+        overflow: hidden;
+        margin-right: 10px;
+        }
+        #search input[type="text"]{
+        border:none;
+        padding:8px;
+        outline: none;
+        font-size: 13px;
+        }
+        #search input[type="submit"]{
+        background-color: white;
+        border:none;
+        padding:8px 12px;
+        font-size: 14px;
+        }
+
+        #search input[type="submit"]:hover{
+        background-color: #e0e0e0;
+        }
+        #basket-icon{
+        margin-right: 10px;
+        }
+        #basket-icon a img{
+        width: 20px;
+        height: auto;
+        }
+        
         .messages-container {
             position: absolute;
-            top: 0;
             right: 0;
             margin-top: 10px;
         }
@@ -146,7 +249,11 @@ unset($_SESSION['password_error']);
         }
 
         .tab-content {
-            display: none;
+            padding: 20px;
+            max-width: 400px;
+            width: 100%;
+            margin: auto;
+            margin-top: 50vh;
         }
 
         .password-container{
@@ -221,155 +328,212 @@ unset($_SESSION['password_error']);
     </style>
 </head>
 <body>
-    <div class="tabs">
-        <h1 class="settings-header">Settings</h1>
-        <div class="tab" onclick="showTab('account-details')">Account Details</div>
-        <div class="tab" onclick="showTab('paymentForm')">Payment Methods</div>
-        <button class="logout" onclick="window.location.href='logout.php'">Log Out</button>
+    <header>
+
+    <div class ="logo">  <!--Hadeeqa-The logo for top of page-->
+    <img src="logo.png" alt="Offside Logo">
     </div>
 
+    <div class="top-right-nav">
+    <div id="nav1">
+    <a href="about.html">About Us</a> 
+    <a href="#">Contact Us</a> 
+    <a href="#">Sign In</a>
+    <!-- <a href="#">Account details</a>  -->
+    </div>
 
+    </header> 
 
-    <div class="tab-content" id="account-details">
-        <form method='post' action='update_details.php'>
-            <div class="details-container">
-                <h2>User Details</h2>
+    <div id="nav2">
+    <div class="nav2-center">
+    <a href="products.php?gender%5B%5D=womens">Women</a>
+    <a href="products.php?gender%5B%5D=mens">Men</a> 
+    <a href="#">Accessories</a>
+    </div>
 
-                <!-- Display user details from the Users table -->
-                <div class="detail">
-                    <label>Email: </label>
-                    <input type="email" class="edit-input" name="email" value="<?php echo $user['Email']; ?>">
-                </div>
-                <div class="detail">
-                    <label>Password: </label>
-                    <div class="password-container">
-                        <span class="password-value">******</span>
-                        <button type="button" class="edit-button" onclick="openPasswordForm()">Edit</button>
-                        <div class="messages-container">
-                            <?php if ($passwordError): ?>
-                                <div class="error-message"><?php echo $passwordError; ?></div>
-                            <?php elseif ($passwordSuccess): ?>
-                                <div class="success-message"><?php echo $passwordSuccess; ?></div>
-                            <?php endif; ?>
+    <div class="nav2-right">
+    <div id="search">
+        <form>
+    <input type="text" name="search" placeholder="Search">
+    <input type="submit" value="Enter">
+        </form>
+    </div>
+    <div id="basket-icon">
+    <a href="cart.php"><img src="basket-icon.png" alt="Basket"></a>  
+    </div>
+    </div>
+    </div>
+
+    <div class="messages-container">
+        <?php if ($passwordError): ?>
+        <div class="error-message"><?php echo $passwordError; ?></div>
+            <?php elseif ($passwordSuccess): ?>
+        <div class="success-message"><?php echo $passwordSuccess; ?></div>
+        <?php endif; ?>
+    </div>
+
+    <div class="messages-container">
+        <?php if ($passwordError): ?>
+        <div class="error-message"><?php echo $passwordError; ?></div>
+        <?php elseif ($passwordSuccess): ?>
+        <div class="success-message"><?php echo $passwordSuccess; ?></div>
+        <?php endif; ?>
+    </div>
+        
+    <div class="messages-container">
+        <?php
+        if (isset($_SESSION['errorMessage']) && !empty($_SESSION['errorMessage'])) {
+            echo "<p class='error-message' style='color: red;'>" . $_SESSION['errorMessage'] . "</p>";
+            unset($_SESSION['errorMessage']); // Clear the error message after displaying it
+        }
+        ?>
+    </div>
+
+    <div class="page-container">
+        <div class="tabs">
+            <h1 class="settings-header">Settings</h1>
+            <div class="tab" onclick="showTab('account-details')">Account Details</div>
+            <div class="tab" onclick="showTab('paymentForm')">Payment Methods</div>
+            <button class="logout" onclick="window.location.href='logout.php'">Log Out</button>
+        </div>
+    
+    
+    
+        <div class="tab-content" id="account-details">
+            <form method='post' action='update_details.php'>
+                <div class="details-container">
+                    <h2>User Details</h2>
+    
+                    <!-- Display user details from the Users table -->
+                    <div class="detail">
+                        <label>Email: </label>
+                        <input type="email" class="edit-input" name="email" value="<?php echo $user['Email']; ?>">
+                    </div>
+                    <div class="detail">
+                        <label>Password: </label>
+                        <div class="password-container">
+                            <span class="password-value">******</span>
+                            <button type="button" class="edit-button" onclick="openPasswordForm()">Edit</button>
+                            
                         </div>
                     </div>
+                    <br>
+                    <label>First Name: </label>
+                    <input type="text" class="edit-input" name="firstName" value="<?php echo $user['First Name']; ?>">
+                    <br>
+                    <label>Last Name: </label>
+                    <input type="text" class="edit-input" name="lastName" value="<?php echo $user['Last Name']; ?>">
+                    <br>
+                    <label>Phone: </label>
+                    <input type="tel" pattern="[0-9]{5} [0-9]{6}" placeholder="00000 000000" class="edit-input" name="phone" value="<?php echo $user['Phone Number']; ?>">
+                    <br>
+                    <label>Address</label>
+                    <input type="text" class="edit-input" name="addressLine" value="<?php echo $address['AddressLine'] ?>">
+                    <br>
+                    <label>City</label>
+                    <input type="text" class="edit-input" name="city" value="<?php echo $address['City'] ?>">
+                    <br>
+                    <label>Region</label>
+                    <input type="text" class="edit-input" name="region" value="<?php echo $address['Region'] ?>">
+                    <br>
+                    <label>Postcode</label>
+                    <input type="text" class="edit-input" name="postcode" value="<?php echo $address['Postcode'] ?>">
+                    <br>
+                    <label>Country</label>
+                    <input type="text" class="edit-input" name="country" value="<?php echo $address['Country'] ?>">
+                    <br>
+    
+                    <!-- Save Changes button -->
+                    <button class="save-button">Save Changes</button>
+                    <input type="hidden" name="submitted" value="TRUE" />
+                </div>
+            </form>
+    
+                <script>
+                    // Clear password input fields and display the passwordForm popup
+                    document.getElementById("currentPassword").value = "";
+                    document.getElementById("newPassword").value = "";
+                    document.getElementById("confirmPassword").value = "";
+                    document.getElementById("passwordForm").style.display = "block";
+                </script>
+    
+    
+            <div class="form-popup" id="passwordForm">
+            <form id="updatePasswordForm" method="post" action="update_password.php" class="form-container">
+                <h1>Update Password</h1>
+    
+                <div>
+                    <label for="currentPassword"><b>Current Password</b></label>
+                    <input type="password" placeholder="Enter Current Password" name="currentPassword" required>
                 </div>
                 <br>
-                <label>First Name: </label>
-                <input type="text" class="edit-input" name="firstName" value="<?php echo $user['First Name']; ?>">
+                <div>
+                    <label for="newPassword"><b>New Password</b></label>
+                    <input type="password" placeholder="Enter New Password" name="newPassword" required>
+                </div>
                 <br>
-                <label>Last Name: </label>
-                <input type="text" class="edit-input" name="lastName" value="<?php echo $user['Last Name']; ?>">
+                <div>
+                    <label for="confirmPassword"><b>Confirm Password</b></label>
+                    <input type="password" placeholder="Confirm New Password" name="confirmPassword" required>
+                </div>
                 <br>
-                <label>Phone: </label>
-                <input type="tel" pattern="[0-9]{5} [0-9]{6}" placeholder="00000 000000" class="edit-input" name="phone" value="<?php echo $user['Phone Number']; ?>">
-                <br>
-                <label>Address</label>
-                <input type="text" class="edit-input" name="addressLine" value="<?php echo $address['AddressLine'] ?>">
-                <br>
-                <label>City</label>
-                <input type="text" class="edit-input" name="city" value="<?php echo $address['City'] ?>">
-                <br>
-                <label>Region</label>
-                <input type="text" class="edit-input" name="region" value="<?php echo $address['Region'] ?>">
-                <br>
-                <label>Postcode</label>
-                <input type="text" class="edit-input" name="postcode" value="<?php echo $address['Postcode'] ?>">
-                <br>
-                <label>Country</label>
-                <input type="text" class="edit-input" name="country" value="<?php echo $address['Country'] ?>">
-                <br>
-
-                <!-- Save Changes button -->
-                <button class="save-button">Save Changes</button>
-                <input type="hidden" name="submitted" value="TRUE" />
+                <button type="submit" class="btn">Save Changes</button>
+                <button type="button" class="btn cancel" onclick="closePasswordForm()">Close</button>
+            </form>
             </div>
-        </form>
-
-            <script>
-                // Clear password input fields and display the passwordForm popup
-                document.getElementById("currentPassword").value = "";
-                document.getElementById("newPassword").value = "";
-                document.getElementById("confirmPassword").value = "";
-                document.getElementById("passwordForm").style.display = "block";
-            </script>
-
-
-        <div class="form-popup" id="passwordForm">
-        <form id="updatePasswordForm" method="post" action="update_password.php" class="form-container">
-            <h1>Update Password</h1>
-
-            <div>
-                <label for="currentPassword"><b>Current Password</b></label>
-                <input type="password" placeholder="Enter Current Password" name="currentPassword" required>
-            </div>
-            <br>
-            <div>
-                <label for="newPassword"><b>New Password</b></label>
-                <input type="password" placeholder="Enter New Password" name="newPassword" required>
-            </div>
-            <br>
-            <div>
-                <label for="confirmPassword"><b>Confirm Password</b></label>
-                <input type="password" placeholder="Confirm New Password" name="confirmPassword" required>
-            </div>
-            <br>
-            <button type="submit" class="btn">Save Changes</button>
-            <button type="button" class="btn cancel" onclick="closePasswordForm()">Close</button>
-        </form>
         </div>
-    </div>
-
-
-
-
-    <div class="tab-content" id="paymentForm">
-        <?php if ($paymentMethods === null || empty($paymentMethods)): ?>
-            <p>No payment methods added.</p>
-        <?php else: ?>
-            <?php foreach ($paymentMethods as $paymentMethod): ?>
-                <div class="payment-method">
-                    <label>Card Number:</label>
-                    <div class="payment-container">
-                        <div class="edit-input">Card ending 
-                         <?php echo substr($paymentMethod['AccountNumber'], -4); ?>
-                        <br>
-                        Expires: <?php echo substr($paymentMethod['ExpiryDate'], -4); ?></div>
-                        <form method="post" action="update_payment.php">
-                            <input type="hidden" name="PaymentID" value="<?php echo $paymentMethod['PaymentID']; ?>">
-                            <button type="submit" name="delete-payment" class="delete-button">Delete</button>
-                        </form>
+    
+    
+    
+    
+        <div class="tab-content" id="paymentForm">
+            <?php if ($paymentMethods === null || empty($paymentMethods)): ?>
+                <p>No payment methods added.</p>
+            <?php else: ?>
+                <?php foreach ($paymentMethods as $paymentMethod): ?>
+                    <div class="payment-method">
+                        <label>Card Number:</label>
+                        <div class="payment-container">
+                            <div class="edit-input">Card ending 
+                             <?php echo substr($paymentMethod['AccountNumber'], -4); ?>
+                            <br>
+                            Expires: <?php echo substr($paymentMethod['ExpiryDate'], -4); ?></div>
+                            <form method="post" action="update_payment.php">
+                                <input type="hidden" name="PaymentID" value="<?php echo $paymentMethod['PaymentID']; ?>">
+                                <button type="submit" name="delete-payment" class="delete-button">Delete</button>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            <?php endforeach; ?>
-        <?php endif; ?>
-
-        <div class="add-payment-button" onclick="openPaymentForm()">Add Payment Method</div>
+                <?php endforeach; ?>
+            <?php endif; ?>
+    
+            <div class="add-payment-button" onclick="openPaymentForm()">Add Payment Method</div>
+        </div>
+    
+        <div class="form-popup" id="paymentPopup">
+        <form method="post" action="update_payment.php" class="form-container">
+            <h1>Add Payment Method</h1>
+    
+            <div>
+                <label for="accountNumber"><b>Card Number</b></label>
+                <input type="text" placeholder="Card Number" name="accountNumber" required>
+            </div>
+            <br>
+            <div>
+                <label for="expirationDate"><b>ExpirationDate</b></label>
+                <input type="text" placeholder="MM/YY" name="expirationDate" required>
+            </div>
+            <br>
+            <div>
+                <label for="cvv"><b>CVV</b></label>
+                <input type="text" placeholder="CVV" name="cvv" required>
+            </div>
+            <br>
+            <button type="submit" class="btn">Add Payment Method</button>
+            <input type="hidden" name="submitted" value="TRUE" />
+            <button type="button" class="btn cancel" onclick="closePaymentForm()">Close</button>
+        </form>
     </div>
-
-    <div class="form-popup" id="paymentPopup">
-    <form method="post" action="update_payment.php" class="form-container">
-        <h1>Add Payment Method</h1>
-
-        <div>
-            <label for="accountNumber"><b>Card Number</b></label>
-            <input type="text" placeholder="Card Number" name="accountNumber" required>
-        </div>
-        <br>
-        <div>
-            <label for="expirationDate"><b>ExpirationDate</b></label>
-            <input type="text" placeholder="MM/YY" name="expirationDate" required>
-        </div>
-        <br>
-        <div>
-            <label for="cvv"><b>CVV</b></label>
-            <input type="text" placeholder="CVV" name="cvv" required>
-        </div>
-        <br>
-        <button type="submit" class="btn">Add Payment Method</button>
-        <input type="hidden" name="submitted" value="TRUE" />
-        <button type="button" class="btn cancel" onclick="closePaymentForm()">Close</button>
-    </form>
 </div>
 
     <script>
