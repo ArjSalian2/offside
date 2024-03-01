68298ab086c3ef25af0093fd79a69b32f5b6fc91
Muhammad Shuayb <210077568@aston.ac.uk>
2024-03-01 14:01:47 +0000

    Admin Inventory UI

---
+++ code/Admin Inventory/nav.php
@@ -0,0 +1,11 @@
+<div class="content">
+  <nav>
+
+    <span class="separator"></span>
+    <ul class="logout">
+      <li>
+        <a href="#"><i class="bx bx-log-out icon"></i> Logout</a>
+      </li>
+    </ul>
+  </nav>
+</div>
