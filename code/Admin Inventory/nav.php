3a4d4f24f136ac6e88d9f7f83976d30ed699e96e
Muhammad Shuayb <210077568@aston.ac.uk>
2024-03-07 21:26:09 +0000

    Updated Nav + Sidebar

--- code/Admin Inventory/nav.php
+++ code/Admin Inventory/nav.php
@@ -1,6 +1,5 @@
 <div class="content">
   <nav>
-
     <span class="separator"></span>
     <ul class="logout">
       <li>
@@ -8,4 +7,41 @@
       </li>
     </ul>
   </nav>
+  <?php if (isset($_SESSION['success'])) { ?>
+    <div class="alert alert-success">
+      <?php echo $_SESSION['success']; ?>
+    </div>
+  <?php } ?>
+  <?php if (isset($_SESSION['error'])) { ?>
+    <div class="alert alert-danger">
+      <?php echo $_SESSION['error']; ?>
+    </div>
+  <?php } ?>
 </div>
+<?php
+unset($_SESSION['success']);
+unset($_SESSION['error']);
+?>
+
+<script>
+  function hideAlert() {
+    const alertElement = document.getElementsByClassName("alert")[0];
+    if (alertElement) {
+      // First, make the alert fade in by adding the 'fade-in' class
+      alertElement.classList.add('fade-in');
+
+      // Then, wait for 3 seconds before starting the fade-out
+      setTimeout(function () {
+        // Begin the fade out by removing the 'fade-in' class
+        alertElement.classList.remove('fade-in');
+
+        // Optionally, if you want to completely remove the alert after fading out,
+        // wait for another 2 seconds (the duration of the fade effect) and then hide it
+        setTimeout(() => {
+          alertElement.style.display = 'none';
+        }, 2000); // Make sure this matches your CSS transition duration
+      }, 3000); // Display time before starting fade-out
+    }
+  }
+  window.onload = hideAlert;
+</script>
--- code/Admin Inventory/sidebar.php
+++ code/Admin Inventory/sidebar.php
@@ -5,16 +5,16 @@
 
   <ul class="page-buttons">
     <li>
-      <a href="./index.php"><i class="bx bxs-dashboard icon"></i> Dashboard</a>
+      <a href="./dashboard.php"><i class="bx bxs-dashboard icon"></i> Dashboard</a>
     </li>
     <li>
-      <a href="./users.php"><i class="bx bxs-user-pin icon"></i> Customers</a>
+      <a href="./view_customer.php"><i class="bx bxs-user-pin icon"></i> Customers</a>
     </li>
     <li>
-      <a href="./index.php"><i class="bx bxs-box icon"></i> Products</a>
+      <a href="./products.php"><i class="bx bxs-box icon"></i> Products</a>
     </li>
     <li>
-      <a href="./index.php"><i class="bx bxs-shopping-bag icon"></i> Orders</a>
+      <a href="./dashboard.php"><i class="bx bxs-shopping-bag icon"></i> Orders</a>
     </li>
   </ul>
 </div>
