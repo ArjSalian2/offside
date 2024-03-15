9425e1a1b4d3632d6634b6a00341faae5902ac74
Mahmood Shah <210132384@aston.ac.uk>
2024-03-15 15:02:00 +0000

    Dashboard and Admin
    Added content to the dashboard and added admin buttons to the navbar

--- code/products.php
+++ code/products.php
@@ -1,11 +1,23 @@
 <?php
 // Start the session
 session_start();
+
+// Connect to the database
+require_once('<user_files>/connection.php');
+
+$isAdmin = false;
+
 if (!isset($_SESSION['user_id'])) {
     $userId = null;  
 } else {
     $userId = $_SESSION["user_id"];
+    $stmtUser = $db->prepare("SELECT * FROM users WHERE userID = ?");
+    $stmtUser->execute([$userId]);
+    $user = $stmtUser->fetch(PDO::FETCH_ASSOC);
+    // Check if the user is an admin
+    $isAdmin = ($user['user_type'] == 0);
 }
+
 ?>
 
 <!DOCTYPE html>
