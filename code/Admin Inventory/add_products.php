d705fe7ecae0385f5d18f71e4dec8317561bf571
Arjun Salian <210101922@aston.ac.uk>
2024-03-24 14:22:29 +0000

    Corrected links to stylesheet

--- code/Admin Inventory/add_products.php
+++ code/Admin Inventory/add_products.php
@@ -42,7 +42,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['add_product'])) {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="CSS/style.css" />
     <title>Add Product</title>
 </head>
 
--- code/Admin Inventory/dashboard.php
+++ code/Admin Inventory/dashboard.php
@@ -102,9 +102,9 @@ $resultOrders = $conn->query($sqlOrders);
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css" rel="stylesheet" />
-  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="CSS/style.css" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="CSS/style.css" />
   <link rel="stylesheet" href="./css/alert.css" />
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
