b3b8c72d37c2ba7fba5dc38e536705ceb0042b88
Arjun Salian <210101922@aston.ac.uk>
2024-03-24 14:29:05 +0000

    Updated feedback page

--- code/Feedback/feedback.php
+++ code/Feedback/feedback.php
@@ -1,3 +1,26 @@
+<?php
+// Start the session
+session_start();
+
+// Connect to the database
+require_once('../user_files/connection.php');
+
+$isAdmin = false;
+
+if (!isset($_SESSION['user_id'])) {
+    $userId = null;  
+} else {
+    $userId = $_SESSION["user_id"];
+    $stmtUser = $db->prepare("SELECT * FROM users WHERE userID = ?");
+    $stmtUser->execute([$userId]);
+    $user = $stmtUser->fetch(PDO::FETCH_ASSOC);
+    // Check if the user is an admin
+    $isAdmin = ($user['user_type'] == 0);
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,6 +30,7 @@
     <link rel="icon" href="../homepage-img/logo.png">
     <link href="https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="CSS/style.css" />
+    <link rel="stylesheet" href="../nav-styles.css">
     <title>Feedback</title>
 </head>
 
@@ -21,45 +45,57 @@
 
 
     </script>
-    <header>
 
-        <div class="logo"> <!--Hadeeqa-The logo for top of page-->
-            <a href="/offside/index.php">
-                <img src="homepage-img/logo.png" alt="Offside Logo">
-            </a>
-        </div>
+<header>
 
-        <div class="top-right-nav">
-            <div id="nav1">
-                <a href="../about.php">About Us</a>
-                <a href="../basket/contact.php">Contact Us</a>
-                <a href="../user_files/login.php">Sign In</a>
-                <a href="../user_files/user_details.php">Account details</a>
-                <a href="../basket/my_orders.php">My orders</a>
-            </div>
+<div class="logo"> <!--Hadeeqah -The logo for top of page-->
+    <a href="/offside/index.php">
+        <img src="../homepage-img/logo.png" alt="Offside Logo">
+    </a>
+</div>
 
-    </header>
+<div class="top-right-nav"> <!-- Hadeeqah- Updated the nav bar -->
+    <div id="nav1">
 
-    <!-- nav2-dima -->
-    <div id="nav2">
-        <div class="nav2-center">
-            <a href="../products.php?gender%5B%5D=womens">Women</a>
-            <a href="../products.php?gender%5B%5D=mens">Men</a>
-            <a href="../products.php?category%5B%5D=accessories">Accessories</a>
-        </div>
+        <?php if ($isAdmin): ?>
+        <a href="../Admin Inventory/dashboard.php">Admin</a>
+        <?php endif; ?>
 
-        <div class="nav2-right">
-            <div id="search">
-                <form>
-                    <input type="text" name="search" placeholder="Search">
-                    <input type="submit" value="Enter">
-                </form>
-            </div>
-            <div id="basket-icon">
-                <a href="../basket/cart.php"><img src="homepage-img/basket-icon.png" alt="Basket"></a>
-            </div>
-        </div>
+        <a href="../about.php">About Us</a>
+        <a href="../basket/contact.php">Contact Us</a>
+        <a href="../user_files/login.php">Log In</a>
+        <a href="../user_files/user_details.php">Account details</a>
+        <a href="../view_orders.php">My orders</a>
+
     </div>
+</div>
+
+
+</header>
+<hr>
+
+<!-- nav2-dima -->
+<div id="nav2">
+<div class="nav2-center">
+    <a href="../products.php?gender%5B%5D=womens">Women</a>
+    <a href="../products.php?gender%5B%5D=mens">Men</a>
+    <a href="../products.php?category%5B%5D=accessories">Accessories</a>
+</div>
+
+<div class="nav2-right">
+    <div id="search">
+        <form>
+            <input type="text" name="search-term" placeholder="Search">
+            <input type="submit" value="Enter">
+        </form>
+    </div>
+    <div id="basket-icon">
+        <a href="../new_cart.php"><img src="../homepage-img/basket-icon.png" alt="Basket"></a>
+    </div>
+</div>
+</div> 
+
+
     <section class="main-container">
         <div class="container">
             <div class="wrapper">
@@ -106,6 +142,6 @@
 
 
 </body>
-<script src="scripts.js"></script>
+<script src="script.js"></script>
 
 </html>
